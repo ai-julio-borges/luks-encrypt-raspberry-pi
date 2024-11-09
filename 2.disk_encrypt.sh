1c18c8a4c02365c16d2ca80634bbc9209560fc48
ai-julio-borges <128798104+ai-julio-borges@users.noreply.github.com>
2024-11-09 17:03:20 +0100

    Update 2.disk_encrypt.sh

--- 2.disk_encrypt.sh
+++ 2.disk_encrypt.sh
@@ -8,25 +8,28 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+# Install necessary packages
 apt-get install busybox cryptsetup initramfs-tools -y
+apt-get install cryptsetup-initramfs -y
 apt-get install expect --no-install-recommends -y
+
+# Copy custom hooks and rebuild initramfs
 cp /boot/install/initramfs-rebuild /etc/kernel/postinst.d/initramfs-rebuild
 cp /boot/install/resize2fs /etc/initramfs-tools/hooks/resize2fs
 chmod +x /etc/kernel/postinst.d/initramfs-rebuild
 chmod +x /etc/initramfs-tools/hooks/resize2fs
 
 echo 'CRYPTSETUP=y' | tee --append /etc/cryptsetup-initramfs/conf-hook > /dev/null
-mkinitramfs -o /boot/initramfs.gz
+mkinitramfs -o /boot/firmware/initramfs.gz
 
-lsinitramfs /boot/initramfs.gz | grep -P "sbin/(cryptsetup|resize2fs|fdisk|dumpe2fs|expect)"
-#Make sure you see sbin/resize2fs, sbin/cryptsetup, and sbin/fdisk in the output.
+lsinitramfs /boot/firmware/initramfs.gz | grep -P "sbin/(cryptsetup|resize2fs|fdisk|dumpe2fs|expect)"
 
-echo 'initramfs initramfs.gz followkernel' | tee --append /boot/config.txt > /dev/null
+echo 'initramfs initramfs.gz followkernel' | tee --append /boot/firmware/config.txt > /dev/null
 
-sed -i '$s/$/ cryptdevice=\/dev\/mmcblk0p2:sdcard/' /boot/cmdline.txt
+sed -i '$s/$/ cryptdevice=\/dev\/mmcblk0p2:sdcard/' /boot/firmware/cmdline.txt
 
-ROOT_CMD="$(sed -n 's|^.*root=\(\S\+\)\s.*|\1|p' /boot/cmdline.txt)"
-sed -i -e "s|$ROOT_CMD|/dev/mapper/sdcard|g" /boot/cmdline.txt
+ROOT_CMD="$(sed -n 's|^.*root=\(\S\+\)\s.*|\1|p' /boot/firmware/cmdline.txt)"
+sed -i -e "s|$ROOT_CMD|/dev/mapper/sdcard|g" /boot/firmware/cmdline.txt
 
 FSTAB_CMD="$(blkid | sed -n '/dev\/mmcblk0p2/s/.*\ PARTUUID=\"\([^\"]*\)\".*/\1/p')"
 sed -i -e "s|PARTUUID=$FSTAB_CMD|/dev/mapper/sdcard|g" /etc/fstab
@@ -34,4 +37,3 @@ sed -i -e "s|PARTUUID=$FSTAB_CMD|/dev/mapper/sdcard|g" /etc/fstab
 echo 'sdcard /dev/mmcblk0p2 none luks' | tee --append /etc/crypttab > /dev/null
 
 echo "Done. Reboot with: sudo reboot"
-#reboot
